489e388cc706234011013c1e6b75227ecb5d0db6
Brennan Hatton MSI <bernnan@equalreality.com>
2023-02-03 10:07:57 +1100

    Making the game restarter

---
+++ Assets/ReceiveChangeSceneEvent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+namespace BrennanHatton.Networking.Events
+{
+	
+	public class ReceiveChangeSceneEvent : MonoBehaviour, IOnEventCallback
+	{
+		public UnityEvent onReceiveChange = new UnityEvent();
+		public bool changeScene = true;
+		//public NetworkPlacerSpawner PlayerManager;
+		
+		private void OnEnable()
+		{
+			PhotonNetwork.AddCallbackTarget(this);
+		}
+	
+		private void OnDisable()
+		{
+			PhotonNetwork.RemoveCallbackTarget(this);
+		}
+	
+		public void OnEvent(EventData photonEvent)
+		{
+			byte eventCode = photonEvent.Code;
+			
+			if(eventCode == SendEventManager.ChangeSceneEventCode)
+			{
+				object[] data = (object[])photonEvent.CustomData;
+				int id = (int)data[0];
+				int sceneBuildId = (int)data[1];
+				int delay = (int)data[2];
+				
+				onReceiveChange.Invoke();
+				
+				if(changeScene)
+					StartCoroutine(changeSceneAfterTime(sceneBuildId,delay));
+			}
+		}
+		
+		IEnumerator changeSceneAfterTime(int bid, int delay)
+		{
+			yield return new WaitForSeconds(delay);
+			
+			SceneManager.LoadScene(bid);
+		}
+	}
+}
---
+++ Assets/SendChangeSceneEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrennanHatton.Networking.Events
+{
+	public class SendChangeSceneEvent : MonoBehaviour
+	{
+		public int sceneId = 0, delay = 2;
+		
+		public void SendChangeScene()
+		{
+			SendChangeScene(sceneId);
+		}
+		
+		public void SendChangeScene(int _sceneId)
+		{
+			SendChangeScene(_sceneId, delay);
+		}
+		
+		public void SendChangeScene(int _sceneId, int _delay)
+		{
+			sceneId = _sceneId;
+			delay = _delay;
+			
+			SendEventManager.SendChangeScene(sceneId, delay);
+		}
+	}
+}
