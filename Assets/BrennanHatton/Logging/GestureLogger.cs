4e97baa5ba8020481a8646fc6d18892d450181c0
Brennan Hatton MSI <bernnan@brennanhatton.com>
2023-02-05 14:13:29 +1100

    Velocity logger for releasing

--- Assets/BrennanHatton/Logging/GestureLogger.cs
+++ Assets/BrennanHatton/Logging/GestureLogger.cs
@@ -51,7 +51,7 @@ namespace BrennanHatton.Logging
 				
 				lastState = state;
 			}
-	    }
+		}
 	    
 		
 	}
--- Assets/BrennanHatton/Logging/GrabberLogger.cs
+++ Assets/BrennanHatton/Logging/GrabberLogger.cs
@@ -6,16 +6,37 @@ using Photon.Pun;
 
 namespace BrennanHatton.Logging
 {
+	
+	[System.Serializable]
+	public class VelocityLogs
+	{
+		public float velocityMin;
+		public string log;
+		[Range(0,10)]
+		public int relativeImportance = 0;
+		
+		public VelocityLogs(float f, string l, int r)
+		{
+			velocityMin = f;
+			log = l;
+			relativeImportance = r;
+			
+		}
+	}
+	
 	public class GrabberLogger : MonoBehaviour
 	{
 		public Grabber grabber;
 		public string handSide;
 		public Importance importance = Importance.Important;
 		public bool usePhotonName = true;
+		
+		public List<VelocityLogs> velocityLogs = new List<VelocityLogs>();
 		void Reset()
 		{
 			grabber = this.GetComponent<Grabber>();
 			handSide = grabber.HandSide.ToString() + " hand";
+			velocityLogs.Add(new VelocityLogs(0,"Release",0));
 				
 		}
 		
@@ -42,12 +63,33 @@ namespace BrennanHatton.Logging
 			LogAction log = new LogAction(true);
 			if(usePhotonName)
 				log.who = PhotonNetwork.LocalPlayer.NickName;
-			log.did = "Released";
+			log.did =  getVelocityLog();
 			log.what = grabbable.name;
 			log.with = handSide;
 			log.importance = importance;
 			
 			ActionLogger.Instance.Add(log);
+		}
+	    
+		string getVelocityLog()
+		{
+			VelocityLogs highesetMinLog = null;
+			
+			for(int i = 0;i < velocityLogs.Count; i++)
+			{
+				if(highesetMinLog == null)
+					highesetMinLog = velocityLogs[i];
+				else
+				{
+					if(highesetMinLog.velocityMin < velocityLogs[i].velocityMin)
+						highesetMinLog = velocityLogs[i];
+				}
+			}
+			
+			if(highesetMinLog != null)
+				return highesetMinLog.log;
+				
+			return "";
 		}
 	}
 	
